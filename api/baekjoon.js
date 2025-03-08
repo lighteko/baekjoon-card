0846938a539d06fbd679ed74e66501fe99510a7c
lighteko <dev.lighteko@gmail.com>
2025-03-07 22:05:08 -0500

    EDIT

--- api/baekjoon.js
+++ api/baekjoon.js
@@ -190,9 +190,8 @@ function renderSingleColumn({
   <!-- 배경 + 테두리 -->
   <rect
     width="${width}" height="${height}"
-    rx="10"
+    rx="8"
     fill="${bgColor}"
-    stroke="#30363d" stroke-width="2"
   />
 
   <!-- 상단: 티어 + handle -->
