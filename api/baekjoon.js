963d34707e50c11fbc4b09d57cf8f8a779dabb72
Heejoon Yi <67038945+lighteko@users.noreply.github.com>
2025-03-09 19:16:17 -0400

    Update baekjoon.js

--- api/baekjoon.js
+++ api/baekjoon.js
@@ -151,13 +151,13 @@ function renderLeftGaugeCard({
   const cx = 80;
   const cy = 150;
   const circleCircum = 2 * Math.PI * radius;
-  const dashVal = circlePercent * circleCircum;
+  const dashVal = (circlePercent / 100) * circleCircum;
 
   const barX = 20;
   const barY = 260;
   const barWidth = width - 40; 
   const barHeight = 8;
-  const barFillWidth = barPercent * barWidth;
+  const barFillWidth = Math.round(barPercent / 100) * barWidth;
 
   // SMIL
   const circleAnim = `
